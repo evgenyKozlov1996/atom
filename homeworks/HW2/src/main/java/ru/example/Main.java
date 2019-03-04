a3fde2d2f3a271c16c2c71763c326bace457070c
Evgeny Kozlov <kozloveg1996@gmail.com>
2019-03-04 10:46:15 +0300

    Added file provider

---
+++ homeworks/HW2/src/main/java/ru/example/Main.java
@@ -0,0 +1,15 @@
+package ru.example;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Bulls and cows");
+
+        WordProvider wordProvider = new WordProvider();
+        String word = wordProvider.getWord();
+
+        while()
+        System.out.println(word);
+    }
+}
