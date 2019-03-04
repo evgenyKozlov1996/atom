d28556e5d3abbc518c119a4265dc1fe4196d24ce
Evgeny Kozlov <kozloveg1996@gmail.com>
2019-03-04 11:03:58 +0300

    Working on game logic

--- homeworks/HW2/src/main/java/ru/example/Main.java
+++ homeworks/HW2/src/main/java/ru/example/Main.java
@@ -1,6 +1,9 @@
 package ru.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -8,8 +11,27 @@ public class Main {
 
         WordProvider wordProvider = new WordProvider();
         String word = wordProvider.getWord();
-
-        while()
+        System.out.println("Please type any word");
         System.out.println(word);
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        String typedWord = br.readLine();
+        while(!typedWord.equals(word)){
+            int cowsCount = 0;
+            int bullsCount = 0;
+
+            List<Character> typedChars = typedWord.toCharArray();
+            char[] wordChars = word.toCharArray();
+            for(int index = 0; index < typedChars.length; index ++){
+                if(index < wordChars.length && typedChars[index] == wordChars[index]){
+                    bullsCount ++;
+                    typedChars.
+                }
+            }
+
+            System.out.println("Cows: " + cowsCount + ". Bulls: " + bullsCount);
+            System.out.println("Please type another word");
+            typedWord = br.readLine();
+        }
+        System.out.println("Congratulations! You win!");
     }
 }
