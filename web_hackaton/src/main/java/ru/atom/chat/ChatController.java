96f4746e6e9238641a208fe18eaad73107ec4df4
Evgeny Kozlov <kozloveg1996@gmail.com>
2019-04-01 09:14:15 +0300

    Fixed styles

--- web_hackaton/src/main/java/ru/atom/chat/ChatController.java
+++ web_hackaton/src/main/java/ru/atom/chat/ChatController.java
@@ -32,9 +32,9 @@ public class ChatController {
      * curl -X POST -i localhost:8080/chat/login -d "name=I_AM_STUPID"
      */
     @RequestMapping(
-            path = "login",
-            method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+        path = "login",
+        method = RequestMethod.POST,
+        consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<String> login(@RequestParam("name") String name) {
         if (name.length() < 1) {
@@ -55,25 +55,25 @@ public class ChatController {
      * curl -i localhost:8080/chat/chat
      */
     @RequestMapping(
-            path = "chat",
-            method = RequestMethod.GET,
-            produces = MediaType.TEXT_PLAIN_VALUE)
+        path = "chat",
+        method = RequestMethod.GET,
+        produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> chat() {
         return new ResponseEntity<>(messages.stream()
-                .map(Object::toString)
-                .collect(Collectors.joining("\n")),
-                HttpStatus.OK);
+            .map(Object::toString)
+            .collect(Collectors.joining("\n")),
+            HttpStatus.OK);
     }
 
     /**
      * curl -i localhost:8080/chat/online
      */
     @RequestMapping(
-            path = "online",
-            method = RequestMethod.GET,
-            produces = MediaType.TEXT_PLAIN_VALUE)
+        path = "online",
+        method = RequestMethod.GET,
+        produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity online() {
-        String responseBody=String.join("\n", usersOnline.keySet().stream().sorted().collect(Collectors.toList()));
+        String responseBody = String.join("\n", usersOnline.keySet().stream().sorted().collect(Collectors.toList()));
         return ResponseEntity.ok(responseBody);
     }
 
@@ -81,15 +81,16 @@ public class ChatController {
      * curl -X POST -i localhost:8080/chat/logout -d "name=I_AM_STUPID"
      */
     @RequestMapping(
-            path = "logout",
-            method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+        path = "logout",
+        method = RequestMethod.POST,
+        consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity logout(@RequestParam("name") String name) {
-        if(usersOnline.containsKey(name)){
+        if (usersOnline.containsKey(name)) {
             usersOnline.remove(name);
             return ResponseEntity.ok("User " + name + " successfully logged out!");
         }
+        
         return ResponseEntity.badRequest().body("User " + name + " does not exists!");
     }
 
@@ -98,13 +99,13 @@ public class ChatController {
      * curl -X POST -i localhost:8080/chat/say -d "name=I_AM_STUPID&msg=Hello everyone in this chat"
      */
     @RequestMapping(
-            path = "say",
-            method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+        path = "say",
+        method = RequestMethod.POST,
+        consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity say(@RequestParam("name") String name, @RequestParam("msg") String msg) {
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        if(usersOnline.containsKey(name)) {
+        if (usersOnline.containsKey(name)) {
             messages.add("(" + name + ")" + "[" + df.format(new Date()) + "]: " + msg);
             return ResponseEntity.ok("(" + name + ")" + "[" + df.format(new Date()) + "]: " + msg);
         }
